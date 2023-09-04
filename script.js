22509fca1337a8c7ebb7db4b2746dcabcb290773
malit mohamut <60866464+mawlid1431@users.noreply.github.com>
2023-09-04 16:37:43 +0300

    script.js

--- script.js
+++ script.js
@@ -1,1 +1,29 @@
-// Good Luck 💪🏾
+function pluralize(words) {
+  const wordCount = {}; // Create an object to count the occurrences of each word
+  const pluralizedWords = [];
+
+  // Count the occurrences of each word
+  for (let word of words) {
+    if (wordCount[word]) {
+      wordCount[word]++;
+    } else {
+      wordCount[word] = 1;
+    }
+  }
+
+  // Iterate through the words and pluralize if they appear more than once
+  for (let word of words) {
+    if (wordCount[word] > 1) {
+      pluralizedWords.push(word + 's');
+    } else {
+      pluralizedWords.push(word);
+    }
+  }
+
+  return pluralizedWords;
+}
+
+// Example usages:
+console.log(pluralize(["cat", "dog", "cat", "mouse"])); // Output: ["cats", "dog", "mouse"]
+console.log(pluralize(["car", "car", "car"])); // Output: ["cars"]
+
